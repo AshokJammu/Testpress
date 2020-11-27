da6d6a0e36dfd9b3893b5695e64192498f669713
ashokkumarnaidu999@gmail.com <ashokkumarnaidu999@gmail.com>
2020-11-27 10:09:01 +0530

    Display options and implement start quiz function

--- onlineQuiz/script.js
+++ onlineQuiz/script.js
@@ -217,3 +217,196 @@ dashBtn.addEventListener('click', ()=> {
        tbody.append(tr)
     }
 })
+
+
+// Display Questions
+
+function displayOptions() {
+
+  scoreTrack.style.color = 'white'
+  scoreTrack.textContent = score + "/10"
+  attempts = 0
+  loadingDiv.style.display = 'none'
+  optionsDiv.style.display = 'block'
+  question.style.color = "black"
+  question.innerHTML = count + 1 + ". " + allData[count].question
+
+  
+
+
+  if (allData[count].type == "boolean") {
+    option1.style.color = '#fff'
+    option2.style.color = '#fff'
+    option1.textContent = "True"
+    option2.textContent = "False"
+    bol1.style.display = 'none'
+    bol2.style.display = 'none'
+  } else if (allData[count].type == 'multiple') {
+
+    bol1.style.display = 'block'
+    bol2.style.display = 'block'
+
+    allData[count].incorrect_answers.push(allData[count].correct_answer)
+
+    console.log(allData[count].incorrect_answers, "incorrect")
+    console.log(shuffle(allData[count].incorrect_answers))
+
+    shuffle(allData[count].incorrect_answers)
+    for (i = 0; i < 4; i++) {
+
+      labels[i].textContent = allData[count].incorrect_answers[i]
+      labels[i].parentElement.checked = false
+      labels[i].style.color = '#fff'
+    }
+
+  }
+
+  correctAns.textContent = ''
+}
+
+// Start Quiz
+
+startQuiz.addEventListener('click', () => {
+  if (category != '' && difficulty != '') {
+    selectTags.style.display = 'none'
+    score = 0
+    count = 0
+    attempts = 0
+    optionsDiv.style.display = 'block'
+    disScore.innerHTML = ''
+    timerDiv.style.display = 'block'
+    loadFlag = true
+    loadingPara.innerHTML = ''
+    countDown()
+    displayOptions()
+  } else {
+    loadingPara.textContent = "Please Choose Category and Difficulty"
+  }
+
+})
+
+
+// Submit button
+
+submitBtn.addEventListener('click', () => {
+
+  event.preventDefault()
+
+  var radioInput = document.querySelectorAll("input");
+  console.log(attempts, count, "count")
+
+  // var txt = "";
+  var uncheck = 0
+  console.log(event.target.nextSibling)
+
+  if (count  >= 10) {
+
+
+    loadingPara.textContent = 'Succesfully completed the Quiz'
+    displayLandPage()
+
+
+    var status = ''
+    var feedback = ''
+    if (score < 5) {
+      status = "Fail"
+      feedback = 'Reattempt the Quiz'
+      disScore.innerHTML = "Score is " + score + " &#128553;"
+    } else if (score >= 5 && score <= 7) {
+      status = "Satisfactory"
+      feedback = 'Need to Improve'
+      disScore.innerHTML = "Score is " + score + " &#128533;"
+    } else {
+      status = "Excellent"
+      feedback = 'Try new concept'
+      disScore.innerHTML = "Score is " + score + " &#128525;"
+    }
+
+    if (count == 10) {
+      let dashObj = {
+        category: catName,
+        difficulty: difficulty,
+        score: score,
+        status: status,
+        feedback: feedback
+      }
+
+      var localData = JSON.parse(localStorage.getItem('dashArray'))
+      console.log(localData)
+      localData.push(dashObj)
+      localStorage.setItem('dashArray', JSON.stringify(localData))
+    }
+
+
+  }else if (attempts < 2) {
+    for (var i = 0; i < radioInput.length; i++) {
+      if (radioInput[i].checked) {
+        // txt = txt + radioInput[i].value;
+        if (allData[count].correct_answer == radioInput[i].nextSibling.textContent) {
+          count++
+          score++
+          // radioInput[i].nextSibling.setAttribute("class", "correct")
+          radioInput[i].nextSibling.style.color = 'green'
+          clearInterval(timerInterval)
+          displayOptions()
+          countDown()
+        }
+        else {
+          // alert('incorrect')
+          attempts++
+          // radioInput[i].nextSibling.setAttribute("class", "wrong")
+          radioInput[i].nextSibling.style.color = 'tomato'
+          correctAns.style.color = 'cyan'
+          correctAns.textContent = "You have one more attempt"
+          // console.log("you have one more attempt")
+        }
+        radioInput[i].checked = false
+        // uncheck = i
+        // break
+      }
+
+
+      
+    }
+
+    if (attempts == 2) {
+      correctAns.style.color = 'white'
+      correctAns.textContent = "The correct answer is " + allData[count].correct_answer
+    }
+
+  } else {
+    console.log(attempts, 'else')
+    clearInterval(timerInterval)
+    count++
+    attempts = 0
+    displayOptions()
+    countDown()
+  }
+
+
+
+
+  // console.log(txt)
+  // console.log(selectData)
+  // radioInput[uncheck].checked = false
+
+})
+
+function countDown() {
+  var time = 0
+  if (difficulty == 'easy') {
+    time = 30
+    timerCountDown(time)
+  } else if (difficulty == 'medium') {
+    time = 45
+    timerCountDown(time)
+  } else if (difficulty == 'hard') {
+    time = 60
+    timerCountDown(time)
+  }
+}
+
+if (!loadFlag) {
+  optionsDiv.style.display = 'none'
+  loadingPara.textContent = 'Loading please wait...'
+}
